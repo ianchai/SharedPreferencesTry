c5893684cefbc3a380d86fc49728ccb13bcf8d47
Ian Chai <ianchai@mmu.edu.my>
2021-09-06 18:36:50 +0800

    I think I have done it but not yet tested.

--- app/src/main/java/com/example/tutorial3sample/MainActivity.kt
+++ app/src/main/java/com/example/tutorial3sample/MainActivity.kt
@@ -7,18 +7,21 @@ import android.graphics.drawable.AnimationDrawable
 import android.view.View
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var frameAnimation:AnimationDrawable
+    val picture:IntArray=intArrayOf(R.drawable.f1,R.drawable.f2,R.drawable.f3,R.drawable.f4,R.drawable.f5)
+    lateinit var pictureView:ImageView
+    var picNum=0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         // Get the Image View
         val view = findViewById<ImageView>(R.id.imageGallery)
         // Setting animation_list.xml as the foreground of the image view
-    }
-
-    fun backFunction(view:View) {
+        pictureView.setBackgroundResource(picture[picNum])
     }
 
     fun nextFunction(view:View) {
+        picNum=(picNum+1)%picture.size
+        pictureView.setBackgroundResource(picture[picNum])
     }
 }
