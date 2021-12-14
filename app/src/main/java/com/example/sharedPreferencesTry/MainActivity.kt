e49b76a7e6054c1509e45781ac1a48fff545a70c
Ian Chai <ianchai@mmu.edu.my>
2021-12-14 16:15:58 +0800

    Show picture number

--- app/src/main/java/com/example/sharedPreferencesTry/MainActivity.kt
+++ app/src/main/java/com/example/sharedPreferencesTry/MainActivity.kt
@@ -6,12 +6,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     val picture:IntArray=intArrayOf(R.drawable.f1,R.drawable.f2,R.drawable.f3,R.drawable.f4,R.drawable.f5)
     lateinit var pictureView:ImageView
     var picNum=0
+    lateinit var showPicNum:TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,11 +26,14 @@ class MainActivity : AppCompatActivity() {
         pictureView = findViewById<ImageView>(R.id.imageGallery)
         // Setting animation_list.xml as the foreground of the image view
         pictureView.setBackgroundResource(picture[picNum])
+        showPicNum = findViewById(R.id.picNumIndicator)
+        showPicNum.setText("Picture number "+picNum)
     }
 
     fun nextFunction(view:View) {
         picNum=(picNum+1)%picture.size
         pictureView.setBackgroundResource(picture[picNum])
+        showPicNum.setText("Picture number "+picNum)
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
